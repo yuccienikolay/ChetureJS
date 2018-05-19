8e0e927bf596b8ce1fb6e37cae914d99b0ce8ce7
nikolay <artelbear@gmail.com>
2018-05-19 22:58:28 +0300

    project tree and global&local import

--- src/Chetrack.js
+++ src/Chetrack.js
@@ -1,1 +1,32 @@
-console.log("sheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet");
+function checkBrowser() {
+    if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) {
+        return 'Opera';
+    } else if(navigator.userAgent.indexOf("Chrome") != -1 ) {
+        return 'Chrome';
+    } else if(navigator.userAgent.indexOf("Safari") != -1 ) {
+        return 'Safari';
+    } else if(navigator.userAgent.indexOf("Firefox") != -1 ) {
+        return 'Firefox';
+    } else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) {
+        return 'IE';
+    } else {
+        return 'unknown';
+    }
+}
+
+
+/////////////////////////////////
+
+export function build(argument) {
+	alert(SizeDict({r:"r"}));
+}
+
+export function user() {
+	let isNewUser = sessionStorage.getItem('amInew') || false;
+	if (!isNewUser) {
+	    sessionStorage.setItem('amInew', true);
+	    let UserID = localStorage.getItem("userId") || `${checkBrowser()}.${navigator.platform}.${Math.floor(Math.random()*1000000000)}`;
+	    localStorage.setItem("userId", UserID);
+	}
+	return isNewUser, localStorage.getItem("userId")
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-const Chetrack = require('./Chetrack.js');
+// Global utils Functions
+
+global.utils = {};
+
+global.utils.CheckTime = function(i) {
+    if (i < 10) {
+    	i = "0" + i
+    };
+    return i
+}
+
+
+// Global Functions
+
+global.Print = function(argument) {
+	console.log(argument)
+}
+
+global.MakeNormal = function(placeholder) {
+  var normalString = this.ReplaceAll(".", placeholder);
+  var toChange = " .,./.\\.$.@.#.%.^.&.*.(.).-.{.}.[.].=.+.?.<.>.~.;._".split(".");
+  toChange.forEach( function (element, index, array) {
+    normalString = normalString.ReplaceAll(element, placeholder);
+  });
+  return normalString;
+}
+
+global.ToClassicDate = function(timestamp) {
+  var date = new Date();
+  date.setTime(timestamp);
+  date.setHours(date.getHours());
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+  var d = date.getDay();
+  var ms = date.getMonth();
+  var y = date.getFullYear();
+  m = CheckTime(m);
+  s = CheckTime(s);
+  return h + ":" + m + ":" + s + " " + d + "." + ms + "." + y;
+}
+
+global.SizeDict = function(dictionary) {
+	return Object.keys(dictionary).length
+}
+
+function function_name(argument) {
+
+}
+
+
+// import
+
+import {user, build} from './Chetrack.js';
+
+
+// Test
+
+Print(user());
+Print(build);
